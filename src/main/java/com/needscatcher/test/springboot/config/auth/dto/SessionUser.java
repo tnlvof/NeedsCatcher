b2fd0568330a5b27b3566a8b54481a5ca332639f
ljs <tnlvof2@naver.com>
2021-01-12 23:00:14 +0900

    구글 로그인 기능 개발중

---
+++ src/main/java/com/needscatcher/test/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.needscatcher.test.springboot.config.auth.dto;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(String name, String email, String picture) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+}
