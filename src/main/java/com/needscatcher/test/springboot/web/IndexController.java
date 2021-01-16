dcf043c4372f0eb9a5abfc6fad527a3f74717811
ljs <tnlvof2@naver.com>
2021-01-17 00:02:42 +0900

    네이버 로그인 연동 추가

--- src/main/java/com/needscatcher/test/springboot/web/IndexController.java
+++ src/main/java/com/needscatcher/test/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.needscatcher.test.springboot.web;
 
+import com.needscatcher.test.springboot.config.auth.LoginUser;
 import com.needscatcher.test.springboot.config.auth.dto.SessionUser;
 import com.needscatcher.test.springboot.service.posts.PostsService;
 import com.needscatcher.test.springboot.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
