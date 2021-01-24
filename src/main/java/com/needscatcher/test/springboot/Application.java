85ab1c482c0373618e9d14cf3c3a83ec1c3cc2b2
tnlvof <tnlvof2@naver.com>
2021-01-24 21:13:28 +0900

    Travis Ci 야 왜 push를 인식 못 하니..?

---
+++ src/main/java/com/needscatcher/test/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.needscatcher.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class Application {
+    //travis test
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/needscatcher/test/springboot/service/posts/PostsService.java
@@ -0,0 +1,52 @@
+package com.needscatcher.test.springboot.service.posts;
+
+import com.needscatcher.test.springboot.domain.posts.Posts;
+import com.needscatcher.test.springboot.domain.posts.PostsRepository;
+import com.needscatcher.test.springboot.web.dto.PostsListResponseDto;
+import com.needscatcher.test.springboot.web.dto.PostsResponseDto;
+import com.needscatcher.test.springboot.web.dto.PostsSaveRequestDto;
+import com.needscatcher.test.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+}
